3b1672abedd3aa1acce89869665426f012a8930a
anujeshyadav <114899681+anujeshyadav@users.noreply.github.com>
2022-12-16 19:09:07 +0530

    new

---
+++ build/static/js/239.dd2e0abf.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[239],{2461:function(a,s){}}]);
+//# sourceMappingURL=239.dd2e0abf.chunk.js.map
---
+++ build/static/js/264.97da2ee0.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[264],{2356:function(a,s){}}]);
+//# sourceMappingURL=264.97da2ee0.chunk.js.map
