768bac2916055e32ef1f8080257fd204f78ff607
anujeshyadav <114899681+anujeshyadav@users.noreply.github.com>
2022-12-26 16:48:33 +0530

    26/12/2022

---
+++ build/static/js/252.52d36c90.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[252],{2452:function(a,s){}}]);
+//# sourceMappingURL=252.52d36c90.chunk.js.map
