33b250820ecccfa93ad8c66e28cc9c03757e05d1
Tirth Borad <tirth.smarttechnica@gmail.com>
2023-06-05 16:41:45 +0530

    make build

---
+++ build/static/js/249.2b992a6f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[249],{2527:function(a,s){}}]);
+//# sourceMappingURL=249.2b992a6f.chunk.js.map
---
+++ build/static/js/269.408c7f7c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[269],{2433:function(a,s){}}]);
+//# sourceMappingURL=269.408c7f7c.chunk.js.map
